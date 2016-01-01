e66fcbab8a361779cbc5d125854b8f67358d36a8
Aaron DeVera <aaronsdevera@protonmail.ch>
2016-01-01 01:14:09 -0800

    Update killzone.sh

--- killzone.sh
+++ killzone.sh
@@ -6,8 +6,8 @@
 # requires knowledge of target network and MAC address
 # allows 3 command line argument for time between deauth attack (in seconds)
 #
-# usage: ./killzone.sh <target network BSSID> <target vendor MAC> <time to wait>
-# ex: ./killzone.sh myNetwork evilHardware 0
+# usage: ./killzone.sh <target network BSSID> <target vendor MAC> <interface> <time to wait>
+# ex: ./killzone.sh myNetwork evilHardware wlan0 1
 #
 # note: manual enumeration increases attack accuracy. beware MAC randomization
 #
